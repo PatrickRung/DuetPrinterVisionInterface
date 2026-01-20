fd9c47ead140f4b60e5986aa7962aee40a28e286
Patrick Rungruengwatanachai <patrick.rung23@gmail.com>
2026-01-20 09:40:36 -0800

    fix typescript compilation issue by adding a tsconfig.json

--- clientInterface/mobiprint-contprint-ui/src/index.js
+++ clientInterface/mobiprint-contprint-ui/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LiveMapPage from "./map/LiveMapPage";
+import LiveMapPage from "./map/LiveMapPage.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
